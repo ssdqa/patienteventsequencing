6e5f875007fe47a6598a2c6abdf6eaf1f5ff5f1e
wieandk <wieandk@chop.edu>
2025-12-05 11:41:11 -0500

    apply changes from squba.gen

--- R/pes_process_pcornet.R
+++ R/pes_process_pcornet.R
@@ -79,7 +79,7 @@ pes_process_pcornet <- function(cohort,
   # Prep cohort
 
   cohort_prep <- prepare_cohort(cohort_tbl = cohort_filter, age_groups = age_groups,
-                                codeset = NULL, omop_or_pcornet = 'pcornet') %>%
+                                omop_or_pcornet = 'pcornet') %>%
     group_by(!!! syms(grouped_list))
 
   for(k in 1:length(site_list_adj)) {
